46ee4f7b585eb0006acc39d20b1362b62973f4ed
logongas <lorenzo.profesor@gmail.com>
2024-01-30 20:27:24 +0100

    cambio de puerto ssh

--- bin/webapp.sh
+++ bin/webapp.sh
@@ -691,7 +691,7 @@ fi
 echo Fichero Subido 
 
 echo "Subiendo copia por SFTP $APP_BASE_PATH/database_backup/$FILE_NAME_DIA"
-sshpass -p "$SFTP_BACKUP_PASSWORD" scp -o StrictHostKeyChecking=no "$APP_BASE_PATH/database_backup/$FILE_NAME_DIA" "$SFTP_BACKUP_USER"@"$SFTP_BACKUP_HOST":"$SFTP_BACKUP_ROOT_PATH/$FILE_NAME_DIA"
+sshpass -p "$SFTP_BACKUP_PASSWORD" scp -o StrictHostKeyChecking=no -P "$SFTP_BACKUP_PORT" "$APP_BASE_PATH/database_backup/$FILE_NAME_DIA" "$SFTP_BACKUP_USER"@"$SFTP_BACKUP_HOST":"$SFTP_BACKUP_ROOT_PATH/$FILE_NAME_DIA"
 echo "Terminada copia por SFTP"
 
 rm $APP_BASE_PATH/database_backup/$FILE_NAME_DIA 
